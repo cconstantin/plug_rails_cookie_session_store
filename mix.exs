570db89bfe90fd52b211c7a584c102ca7d0d530a
Kacper Golinski <kacper.golinski@gmail.com>
2019-01-14 11:41:00 +0000

    bump version to 0.3.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule PlugRailsCookieSessionStore.Mixfile do
   def project do
     [
       app: :plug_rails_cookie_session_store,
-      version: "0.2.0",
+      version: "0.3.0",
       elixir: "~> 1.0",
       description: description(),
       package: package(),
